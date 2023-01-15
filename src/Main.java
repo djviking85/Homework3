882146c21c5b70ae27d2639375a96afd413693b8
Viking <djviking@yandex.ru>
2023-01-15 22:04:52 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte stick = 10;
+        System.out.println("Значение переменной stick типом byte равно " + stick);
+        short apples = 10000;
+        System.out.println("Значение переменной apples типом short равно " + apples);
+        int bit = 1000000;
+        System.out.println("Значение переменной bit типом int равно " + bit);
+        long dollars = 10000000000000L;
+        System.out.println("Значение переменной dollars типом long равно " + dollars);
+        float temperature = 36.6F;
+        System.out.println("Значение переменной temperature типом float равно " + temperature);
+        double pi = 3.145_678_966;
+        System.out.println("Значение переменной pi типом double равно " + pi);
+
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
     }
 }
