eff6599ed94ed05cc1c4ec40ff3353e80e9065e9
Viking <djviking@yandex.ru>
2023-01-15 22:12:59 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -25,5 +25,23 @@ public class Main {
     public static void task2 () {
         System.out.println("Задача 2");
 
+    float a = 27.12F;
+    long b = 987_678_965_549L;
+    double c = 2.786;
+    short d = 569;
+    short e = -159;
+    short f = 27897;
+    byte g = 67;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+
+
+
+
     }
 }
